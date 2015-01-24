792746f30e927113f6c009ae7b9df59a940ec541
ELM <millerer@onid.oregonstate.edu>
2015-01-24 14:18:27 -0800

    Finished the file Objects.js after running it through the provided tests and fixing the code accordingly

--- src/objects.js
+++ src/objects.js
@@ -9,14 +9,14 @@
 */
 
 function returnObjectLiteral() {
-  var SnackObj = {
-    type: 'Goldfish',
-    brand: 'Pepperidge Farm',
-    flavor: 'Cheddar',
-    count: 2000,
-  };
-  return SnackObj; //Modify ONLY this line
-  //end your code
+    var SnackObj = {
+        type: 'Goldfish',
+        brand: 'Pepperidge Farm',
+        flavor: 'Cheddar',
+        count: 2000
+    };
+    return SnackObj; //Modify ONLY this line
+    //end your code
 }
 
 /**
@@ -43,17 +43,40 @@ function returnObjectLiteral() {
 */
 
 //your code here
-function MessageLog(user) {
-    this.user = user;
-    this.logmessage = function (messagetext, direction) {
-        this.messagetext = messagetext;
-        this.direction = direction;
-    }
-    this.getsentmessage = function (n) {
-      
-    }
 
-}
+
+var MessageLog = function (user) {
+    var i;//used for loops
+    this.user = user; //username data
+    this.sent_msg_log = ["EMPTY", "EMPTY", "EMPTY", "EMPTY", "EMPTY"]; //5 index array to hold sent messages' text
+    this.last_msg_received = "EMPTY"; //the last message the user received.
+    this.countsent = 0; //number of messages sent
+    this.countrec = 0; //number of messages received
+    this.logMessage = function (messagetext, direction) { //funtion inputs sent messages into sent_msg_log, recived into last_msg_received, and otherwise counts message types
+        if (direction === 0) {//for sent messages, add to array sent_msg_log
+            for (i = 4; i > 0; i--) { //only 5 sent messages are kept, with each new message added shifting the other's indexes by 1. The 5th index message is lost.
+                this.sent_msg_log[i] = this.sent_msg_log[i - 1];
+            }
+            this.sent_msg_log[0] = messagetext;
+            this.countsent += 1;
+        }
+        else if (direction === 1) { //in case of received message, simply place the text into last_msg_received
+            this.countrec += 1;
+            this.last_msg_received = messagetext;
+        }
+    };
+    this.getSentMessage = function (n) {
+        if (n > -1 && n < 5) {
+            return this.sent_msg_log[n];
+        }
+    };
+    this.totalSent = function () { //return number of messages sent
+        return this.countsent;
+    };
+    this.totalReceived = function () {//return number of messages received
+        return this.countrec;
+    };
+};
 //end your code
 
 /**
@@ -62,7 +85,9 @@ function MessageLog(user) {
 * received.
 */
 //your code here
-
+MessageLog.prototype.lastReceivedMessage = function () {
+    return this.last_msg_received;
+};
 //end your code
 
 /**
@@ -72,5 +97,8 @@ function MessageLog(user) {
 */
 
 //your code here
-
+var myLog = new MessageLog("BlackHatGuy");
+myLog.logMessage("foo", 1);
+myLog.logMessage("bar", 1);
+myLog.logMessage("baz", 1);
 //end your code
